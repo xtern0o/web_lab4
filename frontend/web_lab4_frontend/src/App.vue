2e740aefd2ca55e1ba984073d3a2805664666b7f
xtern0o <dos30028@gmail.com>
2025-10-30 22:51:43 +0300

    [feat-frontend]: Less styles; Homepage a little bit; говнокод потому что я еще не разобрался с vue

---
+++ frontend/web_lab4_frontend/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import Home from './components/Home.vue';
+
+
+</script>
+
+<template>
+
+  <div>
+    <header>
+      <div class="container">
+        <div class="navbar">
+          <div class="navbar-brand">
+            <p class="navbar-brand-link">P3211</p>
+            <p class="navbar-brand-link">Карнажицкий Максим Романович</p>
+            <p class="navbar-brand-link">Лаб 4</p>
+            <p class="navbar-brand-link">8281</p>
+
+          </div>
+          <nav class="navbar-nav">
+            <a class="navbar-nav-item-link" href="google.com">Главная</a>
+            <a class="navbar-nav-item-link" href="google.com">Точки</a>
+          </nav>
+          
+        </div>
+      </div>
+    </header>
+    
+    <main>
+      <div class="container">
+        <Home />
+      </div>
+    </main>
+
+    <footer>
+      <div class="container">
+
+      </div>
+    </footer>
+  </div>
+
+</template>
+
+<style lang="less">
+@import './styles/style.less';
+
+</style>
+
---
+++ frontend/web_lab4_frontend/src/components/Home.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Главная</h1>
+    <div class="grid-clock-info">
+        <div>
+            <h3>Часы</h3>
+            <canvas id="clock-canvas"></canvas>
+        </div>
+
+        <!-- <div class="card">
+            <p class="card-header">Часы</p>
+            <div class="card-body">
+                <canvas id="clock-canvas"></canvas>
+            </div>
+        </div> -->
+        
+        <div class="card">
+            <p class="card-header">Че тут есть</p>
+            <div class="card-body">
+                
+               
+            </div>
+        </div>
+    </div>
+</template>
+
