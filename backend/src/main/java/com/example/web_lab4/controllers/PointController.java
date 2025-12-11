e80603985d804d2d1b70928f68c865f2ab08825c
xtern0o <dos30028@gmail.com>
2025-12-11 21:09:52 +0300

    [...]

--- backend/src/main/java/com/example/web_lab4/controllers/PointController.java
+++ backend/src/main/java/com/example/web_lab4/controllers/PointController.java
@@ -5,8 +5,10 @@ import com.example.web_lab4.dto.request.PointRequestDto;
 import com.example.web_lab4.dto.response.PointResponseDto;
 import com.example.web_lab4.entity.UserEntity;
 import com.example.web_lab4.service.PointService;
-import com.example.web_lab4.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +20,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PointController {
     private final PointService pointService;
-    private final UserService userService;
 
     @GetMapping
     public List<PointResponseDto> getPoints(
@@ -30,10 +31,10 @@ public class PointController {
 
     @PostMapping
     public PointResponseDto createPoint(
-            @Validated(OnCreate.class) @RequestBody PointRequestDto requestDTO
+            @Validated(OnCreate.class) @RequestBody PointRequestDto requestDTO,
+            @AuthenticationPrincipal UserDetails currentUser
     ) {
-        // TODO: возможно лучше добавить свою аннотацию на параметр метода для получения текущего пользователя
-        UserEntity currentUser = userService.getCurrentUser();
-        return pointService.createPoint(requestDTO, currentUser);
+        return pointService.createPoint(requestDTO, (UserEntity) currentUser);
     }
+
 }
